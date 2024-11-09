c6d37e648192d08afc8f4c163d96cfdc492b81e2
Ulises Morales González <ulises.m.2024@gmail.com>
2024-11-09 20:20:50 -0300

    Ejemplo Bucle For

---
+++ Ejemplos/EjemploBucleFor/src/ejemplobuclefor/EjemploBucleFor.java
@@ -0,0 +1,14 @@
+
+package ejemplobuclefor;
+
+public class EjemploBucleFor {
+
+    public static void main(String[] args) {
+        
+        for (int contador=0; contador<=10; contador++) {
+            System.out.println("Estoy en la vuelta número: "+contador);
+
+        }
+    }
+    
+}
