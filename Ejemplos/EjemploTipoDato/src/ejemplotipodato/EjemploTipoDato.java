c7569ea05156eb38d1235ddc69639656efeb1e63
Ulises Morales González <ulises.m.2024@gmail.com>
2024-11-09 20:18:19 -0300

    Ejemplo para tipo de dato

---
+++ Ejemplos/EjemploTipoDato/src/ejemplotipodato/EjemploTipoDato.java
@@ -0,0 +1,23 @@
+
+package ejemplotipodato;
+
+public class EjemploTipoDato {
+
+    public static void main(String[] args) {
+        
+        /* Las variables mantienen una estructura camelcase pero la primera palabra es con minuscula siempre.
+        */
+        //Operación de asignación
+        int edad = 22;
+        double estatura = 1.80;
+        boolean tieneCarnetDeConducir = true;
+        char letra = 'L';
+        String direccion = "Los alerces 123";
+        long numeroLargo = 234234234;
+        
+        System.out.println("Hola! Mi edad es: "+ edad);
+        System.out.println("Mi estatura es: "+ estatura);
+        System.out.println("Mi dirección es: "+ direccion);
+    }
+    
+}
